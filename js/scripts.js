9278c1af0c79e833ba52267322fb0af4db105c93
Jeff Ellingham <jeffinghamm@gmail.com>
2023-05-25 19:55:57 -0400

    Added first variable for app dataset

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
-alert('Hello Beautiful World!');
 
-let favoriteFood = 'Pepperoni pizza... or maybe Frosted Flakes. Yogurt?'
-
-document.write(favoriteFood);
-
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 28,
+        type: ['grass', 'poison'],
+        weaknesses: ['fire', 'psychic', 'flying', 'ice']
+    },
+    {
+        name: 'Charmander',
+        height: 24,
+        type: ['fire'],
+        weaknesses: ['water', 'ground', 'rock']
+    },
+    {
+        name: 'Squirtle',
+        height: 20,
+        type: ['water'],
+        weaknesses: ['grass', 'electric']
+    },
+    {
+        name: 'Caterpie',
+        height: 12,
+        type: ['bug'],
+        weaknesses: ['fire', 'flying', 'rock']
+    },
+    {
+        name: 'Weedle',
+        height: 12,
+        type: ['bug', 'poison'],
+        weaknesses: ['fire', 'psychic', 'flying', 'rock']
+    }
+];
