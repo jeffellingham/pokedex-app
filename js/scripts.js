8ecf23335bd428472126e8fb0d36d680190ee3af
Jeff Ellingham <jeffinghamm@gmail.com>
2023-05-27 19:39:11 -0400

    Updated Pokemon list and added basic styles

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,50 @@
 let pokemonList = [
     {
         name: 'Bulbasaur',
-        height: 28,
+        id: 1,
+        height: 0.7,
         type: ['grass', 'poison'],
         weaknesses: ['fire', 'psychic', 'flying', 'ice']
     },
     {
         name: 'Charmander',
-        height: 24,
+        id: 4,
+        height: 0.6,
         type: ['fire'],
         weaknesses: ['water', 'ground', 'rock']
     },
     {
+        name: 'Charmeleon',
+        id: 5,
+        height: 1.1,
+        type: ['fire'],
+        weaknesses: ['water', 'ground', 'rock']
+    },
+    {
+        name: 'Charizard',
+        id: 6,
+        height: 1.7,
+        type: ['fire', 'flying'],
+        weaknesses: ['water', 'electric', 'rock']
+    },
+    {
         name: 'Squirtle',
-        height: 20,
+        id: 7,
+        height: 0.5,
         type: ['water'],
         weaknesses: ['grass', 'electric']
     },
     {
         name: 'Caterpie',
-        height: 12,
+        id: 10,
+        height: 0.3,
         type: ['bug'],
         weaknesses: ['fire', 'flying', 'rock']
     },
     {
         name: 'Weedle',
-        height: 12,
+        id: 13,
+        height: 0.3,
         type: ['bug', 'poison'],
         weaknesses: ['fire', 'psychic', 'flying', 'rock']
     }
