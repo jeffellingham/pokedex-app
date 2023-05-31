77bb75d432f565c97397020d9d3ed9cbff57967e
Jeff Ellingham <jeffinghamm@gmail.com>
2023-05-31 18:15:59 -0400

    Refactored for loop to forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -51,60 +51,81 @@ let pokemonList = [
     }
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    //Go through each object and start with Pokemon's id and name
+pokemonList.forEach(function(pokemon) {
     document.write(
-        "#" + pokemonList[i].id + " " + pokemonList[i].name + "<br>"
+        "<section><p>#" + pokemon.id + " " + pokemon.name + "</p>"
     );
-    //Print each Pokemon's height and text if condition met
-    if (pokemonList[i].height > 1) {
-        document.write(
-            "Height: " + pokemonList[i].height + "m" +
-                "    <-- Wow, that's big!"
-        );
-    } else if (pokemonList[i].height < .5) {
-        document.write(
-            "Height: " + pokemonList[i].height + "m" +
-                "    <-- Aww, it's so tiny!"
-        );
-    } else {
-        document.write(
-            "Height: " + pokemonList[i].height + "m"
-        );
-    }
-    document.write('<br>');
-    //For loops to print each type and additional text if condition met
-    document.write("Type: ");
-    for (let j = 0; j < pokemonList[i].type.length; j++) {
-        document.write(pokemonList[i].type[j]);
-        if ((j+1) < pokemonList[i].type.length) {
-            document.write(', ');
-        }
-        else if ((j+1) === pokemonList[i].type.length) {
-            document.write('.');
-        }
-    }
-    for (let j = 0; j < pokemonList[i].type.length; j++){
-        if (pokemonList[i].type[j] === 'fire'){
-            document.write(' <-- Woah, this one\'s so hawt right now!')
-        }
-        else if (pokemonList[i].type[j] === 'bug') {
-            document.write(' <-- Eww, it\'s a bug!');
-        }
-    }
-    document.write('<br>');
-    //Same for loop for weaknesses, but no conditional text
-    document.write("Weaknesses: ");
-    for (let j = 0; j < pokemonList[i].weaknesses.length; j++) {
-        document.write(pokemonList[i].weaknesses[j]);
-        if ((j+1) < pokemonList[i].weaknesses.length) {
-            document.write(', ');
-        }
-        else if ((j+1) === pokemonList[i].weaknesses.length) {
-            document.write('.');
-        }
-    }
-    document.write('<hr>')
-}
+    document.write(
+        "<p>Height: <span>" + pokemon.height + "m</span></p>"
+    );
+    document.write("<p>Type: ");
+    pokemon.type.forEach(function(types) {
+        document.write("<span>" + types + " - ");
+    });
+    document.write("</span></p>");
+    document.write("<p>Weaknesses: ");
+    pokemon.weaknesses.forEach(function(weakness) {
+        document.write("<span>" + weakness + " - ");
+    });
+    document.write("</span></p><hr></section>");
+});
+
+
+
+// for (let i = 0; i < pokemonList.length; i++) {
+//     //Go through each object and start with Pokemon's id and name
+//     document.write(
+//         "<section>#" + pokemonList[i].id + " " + pokemonList[i].name + "<br>"
+//     );
+//     //Print each Pokemon's height and text if condition met
+//     if (pokemonList[i].height > 1) {
+//         document.write(
+//             "Height: " + pokemonList[i].height + "m" +
+//                 "    <-- Wow, that's big!"
+//         );
+//     } else if (pokemonList[i].height < .5) {
+//         document.write(
+//             "Height: " + pokemonList[i].height + "m" +
+//                 "    <-- Aww, it's so tiny!"
+//         );
+//     } else {
+//         document.write(
+//             "Height: " + pokemonList[i].height + "m"
+//         );
+//     }
+//     document.write('<br>');
+//     //For loops to print each type and additional text if condition met
+//     document.write("Type: ");
+//     for (let j = 0; j < pokemonList[i].type.length; j++) {
+//         document.write(pokemonList[i].type[j]);
+//         if ((j+1) < pokemonList[i].type.length) {
+//             document.write(', ');
+//         }
+//         else if ((j+1) === pokemonList[i].type.length) {
+//             document.write('.');
+//         }
+//     }
+//     for (let j = 0; j < pokemonList[i].type.length; j++){
+//         if (pokemonList[i].type[j] === 'fire'){
+//             document.write(' <-- Woah, this one\'s so hawt right now!')
+//         }
+//         else if (pokemonList[i].type[j] === 'bug') {
+//             document.write(' <-- Eww, it\'s a bug!');
+//         }
+//     }
+//     document.write('<br>');
+//     //Same for loop for weaknesses, but no conditional text
+//     document.write("Weaknesses: ");
+//     for (let j = 0; j < pokemonList[i].weaknesses.length; j++) {
+//         document.write(pokemonList[i].weaknesses[j]);
+//         if ((j+1) < pokemonList[i].weaknesses.length) {
+//             document.write(', ');
+//         }
+//         else if ((j+1) === pokemonList[i].weaknesses.length) {
+//             document.write('.');
+//         }
+//     }
+//     document.write('</section>')
+// }
 
 
